78b30047f40c1da0b87cd9ac91adc7cf95630a86
Alvyn Abranches <alvynabranches@gmail.com>
2021-01-11 00:15:16 +0530

    11012021v1

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from time import perf_counter
 current_location = dirname(abspath(__file__))
 
 s = perf_counter()
+# df = pd.read_excel('./data/indeed_results.xlsx')
 
-
+content = 'with prior experience in Data entry, Call Centre, Admin, Collection Agents, Collection Executives, Customer Support Executive, Office Assistant, Driver, Delivery Boys, Back Office, Teacher, Banking, Accounts, Operator can apply to maximize their earnings up to 25,000 per month.'
+    
+print(f'{content[190:197]=}')
 
 e = perf_counter()
 print(f'Time Taken: {e-s:.2f} seconds')
