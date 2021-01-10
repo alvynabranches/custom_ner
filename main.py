8336514a6de4ae7e951d0e7775ba2cd4b0fe45aa
Alvyn Abranches <alvynabranches@gmail.com>
2021-01-11 02:05:40 +0530

    11012021v7

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ except ModuleNotFoundError:
     subprocess.call('pip install --upgrade --user spacy')
     subprocess.call('python -m spacy en_core_web_sm')
 
-import en_core_web_sm
+import en_core_web_sm, random
 from os.path import dirname, abspath
 from time import perf_counter
 from pandas import read_excel
+from spacy.util import minibatch, compounding
+from spacy import displacy
 current_location = dirname(abspath(__file__))
 
 s = perf_counter()
@@ -66,11 +68,45 @@ train = [
 
 for _, annotations in train:
     for ent in annotations.get('entities'):
-        print(ent[2])
+        ner.add_label(ent[2])
         
 disable_pipes = [pipe for pipe in nlp.pipe_names if pipe != 'ner']
 
+print('Before Training')
+for text, _ in train:
+    doc = nlp(text)
+    print(doc)
+    print(f'Entities: {[(ent.text, ent.label_) for ent in doc.ents]}')
 
+doc = nlp('Official Job posted from SwiggyJoin Swiggy family as Part-Time or a Full-time Food Delivery executive. Who can be an ideal Rider Driver Delivery Boy Logistics AdminDelivery Executive for Swiggy? Minimum qualification - Fresher, 10th pass, 12th pass, Graduate, New Graduates, Trainee Candidates with prior experience in Data entry, Call Centre, Admin, Collection Agents, Collection Executives, Customer Support Executive, Office Assistant, Driver, Delivery Boys, Back Office, Teacher, Banking, Accounts, Operator can apply to maximize their earnings up to 25,000 per month. Documents to be carried for Walk-in: Valid Two-wheeler Driver License and Driver RC book PAN Card or Acknowledgment of PAN application Aadhaar Card or voter ID Benefits: Flexible working hours for security guards, computer operators, account executives night shifts weekend Attractive Weekly, weekend and monthly Incentives for 10th pass, 12th pass')
+for ent in doc.ents:
+    print(f'Text: {ent.text:4}\t\t Start Char: {ent.start_char:4}\t\t End Char: {ent.end_char}\t\t Label: {ent.label_}')
+
+print('Training')
+with nlp.disable_pipes(*disable_pipes):
+    optimizer = nlp.resume_training()
+    
+    for iteration in range(100):
+        random.shuffle(train)
+        losses = {}
+        
+        batches = minibatch(train, size=compounding(1.0, 4.0, 1.001))
+        
+        for batch in batches:
+            text, annotation = zip(*batch)
+            nlp.update(text, annotation, drop=0.5, losses=losses, sgd=optimizer)
+
+            print(f'Losses: {losses}')
+            
+print('After Training')
+for text, _ in train:
+    doc = nlp(text)
+    print(doc)
+    print(f'Entities: {[(ent.text, ent.label_) for ent in doc.ents]}')
+
+doc = nlp('Official Job posted from SwiggyJoin Swiggy family as Part-Time or a Full-time Food Delivery executive. Who can be an ideal Rider Driver Delivery Boy Logistics AdminDelivery Executive for Swiggy? Minimum qualification - Fresher, 10th pass, 12th pass, Graduate, New Graduates, Trainee Candidates with prior experience in Data entry, Call Centre, Admin, Collection Agents, Collection Executives, Customer Support Executive, Office Assistant, Driver, Delivery Boys, Back Office, Teacher, Banking, Accounts, Operator can apply to maximize their earnings up to 25,000 per month. Documents to be carried for Walk-in: Valid Two-wheeler Driver License and Driver RC book PAN Card or Acknowledgment of PAN application Aadhaar Card or voter ID Benefits: Flexible working hours for security guards, computer operators, account executives night shifts weekend Attractive Weekly, weekend and monthly Incentives for 10th pass, 12th pass')
+for ent in doc.ents:
+    print(f'Text: {ent.text:4}\t\t Start Char: {ent.start_char:4}\t\t End Char: {ent.end_char}\t\t Label: {ent.label_}')
 
 e = perf_counter()
 print(f'Time Taken: {e-s:.2f} seconds')
