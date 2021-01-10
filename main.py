0065d3613ce43b83b5351083ac4cf2c65328ebce
Alvyn Abranches <alvynabranches@gmail.com>
2021-01-11 00:23:18 +0530

    11012021v2

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import numpy as np, pandas as pd
 from os.path import dirname, abspath
 from time import perf_counter
+from pandas import read_excel
 current_location = dirname(abspath(__file__))
 
 s = perf_counter()
-# df = pd.read_excel('./data/indeed_results.xlsx')
+# df = read_excel('./data/indeed_results.xlsx')
 
-content = 'with prior experience in Data entry, Call Centre, Admin, Collection Agents, Collection Executives, Customer Support Executive, Office Assistant, Driver, Delivery Boys, Back Office, Teacher, Banking, Accounts, Operator can apply to maximize their earnings up to 25,000 per month.'
+content = 'Documents to be carried for Walk-in: Valid Two-wheeler Driver License and Driver RC book PAN Card or'
     
-print(f'{content[190:197]=}')
+print(f'{content[74:88]=}')
 
 e = perf_counter()
 print(f'Time Taken: {e-s:.2f} seconds')
