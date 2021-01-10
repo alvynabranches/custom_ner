597bd2d78c0733906bf7a91d6ae77ba582fa8408
Alvyn Abranches <alvynabranches@gmail.com>
2021-01-11 01:23:26 +0530

    11012021v4

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-import pandas as pd
+try:
+    pass
+except ModuleNotFoundError:
+    pass
+
+import en_core_web_sm
 from os.path import dirname, abspath
 from time import perf_counter
 from pandas import read_excel
 current_location = dirname(abspath(__file__))
 
 s = perf_counter()
-# df = read_excel('./data/indeed_results.xlsx')
 
-content = 'account executives night shifts weekend Attractive Weekly, weekend and monthly Incentives for 10th pass, 12th pass'
-    
-print(f'{content[94:103]=}')
+df = read_excel('./data/indeed_results.xlsx')
+
+description = df['Description'].dropna().reset_index()['Description']
+pre_processed=description.apply(lambda x: str(x).replace('Job Summary','').replace('Job Description','').replace('Short Description','').replace('\n',' ').replace('(',' ').replace(')',' ').replace('/',' ').replace('|',' ').replace('  ',' ').replace('"',' ').replace("'",' ').replace('  ','').lstrip().rstrip())
+
+nlp = en_core_web_sm.load()
 
 e = perf_counter()
 print(f'Time Taken: {e-s:.2f} seconds')
