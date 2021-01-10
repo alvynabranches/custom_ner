88a8c6acfbb3563fea6756d99d094eb4097107cd
Alvyn Abranches <alvynabranches@gmail.com>
2021-01-10 23:26:20 +0530

    10012021v7

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import os, time, sys, numpy as np, pandas as pd
-current_location = os.path.dirname(os.path.abspath(__file__))
+import numpy as np, pandas as pd
+from os.path import dirname, abspath
+from time import perf_counter
+current_location = dirname(abspath(__file__))
 
+s = perf_counter()
+
+
+
+e = perf_counter()
+print(f'Time Taken: {e-s:.2f} seconds')
